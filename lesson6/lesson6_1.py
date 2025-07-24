a029b3cee7e8b9074043ef72ef4b68de20b15006
philliutwn <phil_liu_twn@hotmail.com>
2025-07-24 13:57:10 +0800

    更新 lesson6_1.py，將 BrowserConfig 的 headless 設定改為 True

--- lesson6/lesson6_1.py
+++ lesson6/lesson6_1.py
@@ -5,8 +5,9 @@ async def main():
     url = 'https://www.wantgoo.com/stock/2317/technical-chart'
     #建立一個BrowserConfig,讓chromium的瀏覽器顯示
     #BrowserConfig實體
+
     browser_config = BrowserConfig(
-        headless=False
+        headless=True
     )
     stock_schema = {
         "name": "StockInfo",
@@ -94,3 +95,4 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
+    
