774f3fa17c19c1f98d2574af9fd2bf7cb0c6f061
philliutwn <phil_liu_twn@hotmail.com>
2026-05-06 11:32:04 +0800

    Add initial implementation of Site class and JSON file handling comments

---
+++ lesson3/test3.py
@@ -0,0 +1,10 @@
+# 1. Practice how to establish a class
+# 2. Practice how to use with ... as 
+# 3. How to open a Jason file
+# 4. How to grab the data in the Jason file
+
+
+class Site():
+    def __init__(self):
+        self.
+
